e84c65ebeff5a5472bd5f96ec7b5690e1e9e9e0d
Iury Xavier Bomfim <111296130+Xaviend@users.noreply.github.com>
2024-08-14 10:49:18 -0300

    Update Programa.c

--- Aulas/aula01/Programa.c
+++ Aulas/aula01/Programa.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(){
- 
+int main() {
+  int a, b, soma;
+  printf("Digite o primeiro número: ");
+  scanf("%d", &a);
+  printf("Digite o segundo número: ");
+  scanf("%d", &b);
+  soma = a + b;
+  printf("A soma dos números %d e %d é igual a: %d",a,b,soma);
+
   return 0;
-} 
+}
