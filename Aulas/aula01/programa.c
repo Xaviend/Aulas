5de03f597e5770754697ddd4618832aa904a2554
Iury Xavier Bomfim <111296130+Xaviend@users.noreply.github.com>
2024-10-02 09:39:08 -0300

    programa.c

---
+++ Aulas/aula01/programa.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  printf("Texto generico\n");
+  printf("%i %i \n", 10,22);
+  printf("%f\n",10.25);
+  printf("%10.2f\n",10.250999);
+  // caractere de aspas simples 
+  printf("%c\n", 'a');
+
+  //impime uma string 
+  printf("%s\n","Oi, Bom dia ");
+  return 0;
+}
